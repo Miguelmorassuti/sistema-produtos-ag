9733ecd3351bb1f3f39a73698130216f0b7bac34
Miguel Morassuti <miguelguimaraesmorassuti@hotmail.com>
2024-12-27 22:46:06 -0300

    iniciando tela home

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Products from "./pages/Products";
 import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/produtos" element={<Products />} />
         <Route path="/perfil" element={<div>Perfil</div>} />
         <Route path="/configuracoes" element={<div>Configurações</div>} />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,71 +1,31 @@
 import React from "react";
-import { object, string } from "yup";
-import { useForm } from "react-hook-form";
-import { Box, Button, Link, TextField, Typography } from "@mui/material";
+import { Box, Grid2, Typography } from "@mui/material";
+import Drawer from "../Products/components/Drawer";
 
-import "./styles.css";
-
-function Home() {
-  const schema = object().shape({
-    username: string().required("Username is required"),
-    password: string().required("Password is required"),
-  });
-
-  const { register, handleSubmit, errors } = useForm({
-    validationSchema: schema,
-  });
-
-  const onSubmit = (data) => {
-    console.log(data);
-  };
-
+const Products = () => {
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignContent="center"
-      height="100%"
-    >
-      <Box bgcolor="white" width={500} padding={5} borderRadius={5}>
-        <Typography variant="h4" mb={2}>
-          Login
-        </Typography>
-        <Typography>Entre com suas credenciais</Typography>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Box paddingY={2}>
-            <TextField
-              name="username"
-              error={!!errors?.username}
-              label="Nome"
-              helperText={errors?.username ? errors?.username?.message : ""}
-              type="email"
-              // inputRef={register}
-              fullWidth
-            />
-          </Box>
-          <Box pb={2}>
-            <TextField
-              name="password"
-              error={!!errors?.password}
-              label="Senha"
-              // inputRef={register}
-              helperText={errors?.password ? errors?.password?.message : ""}
-              type="password"
-              fullWidth
-            />
-          </Box>
-          <Button color="warning" type="submit" variant="contained" fullWidth>
-            Entrar
-          </Button>
-          <Box pt={2}>
-            <Typography>
-              Esqueceu sua senha? <Link underline="hover">Clique aqui!</Link>
-            </Typography>
-          </Box>
-        </form>
+    <Drawer>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignContent="center"
+        height="100%"
+      >
+        <Grid2 container width="100%">
+          <Grid2 item minWidth={300}>
+            <Box>
+              <Typography>Usúarios</Typography>
+            </Box>
+          </Grid2>
+          <Grid2 item>
+            <Box>
+              <Typography>Produtos</Typography>
+            </Box>
+          </Grid2>
+        </Grid2>
       </Box>
-    </Box>
+    </Drawer>
   );
-}
+};
 
-export default Home;
+export default Products;
